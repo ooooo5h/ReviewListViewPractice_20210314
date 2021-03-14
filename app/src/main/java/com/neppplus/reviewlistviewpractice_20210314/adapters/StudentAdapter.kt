282e6dbccc8cc2064b02a1adcdd2b25dd535142d
전은형 <ooooo5h91@gmail.com>
2021-03-14 11:21:52 +0900

    학생 목록을 뿌려주는 Adapter 생성  - Arrayadapter를 상속받아서 만든다

---
+++ app/src/main/java/com/neppplus/reviewlistviewpractice_20210314/adapters/StudentAdapter.kt
@@ -0,0 +1,7 @@
+package com.neppplus.reviewlistviewpractice_20210314.adapters
+
+import android.widget.ArrayAdapter
+import com.neppplus.reviewlistviewpractice_20210314.datas.Student
+
+class StudentAdapter : ArrayAdapter<Student>() {
+}
